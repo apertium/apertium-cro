2e28f329091c000188a9c485af152c27177049e9
Francis Tyers <ftyers@prompsit.com>
2019-05-26 07:10:58 +0100

    update script

--- texts/print-text.py
+++ texts/print-text.py
@@ -5,7 +5,10 @@ for line in sys.stdin.readlines():
 		continue
 	line = line.strip('\n')
 	line = re.sub(r'\t\t*', r'\t', line)
+	line = re.sub(r'  +', r'\t', line)
 
 	row = line.split('\t')
-	
-	print(row[0], row[1], '\tcro\t', row[2], '\n\teng\t', row[3])
+	csent = row[2].replace('-','')	
+	csent = csent.replace('[','').replace(']','')
+	print('%s %s\tcro\t%s\n\t   \t%s\n\teng\t%s' % (row[0], row[1], csent, row[2], row[3]))
+	print()
