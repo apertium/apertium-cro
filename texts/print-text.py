9abdbdd36fc7627ac9467510dbc2e33008adc9d5
Francis Tyers <ftyers@prompsit.com>
2019-07-11 16:32:47 +0100

    make the script be robust to errors

--- texts/print-text.py
+++ texts/print-text.py
@@ -1,5 +1,5 @@
 import sys, re
-
+lno=1
 for line in sys.stdin.readlines():
 	if line.strip() == '':
 		continue
@@ -8,7 +8,11 @@ for line in sys.stdin.readlines():
 	line = re.sub(r'  +', r'\t', line)
 
 	row = line.split('\t')
+	if len(row) < 4:
+		print('WARNING:%d\t%s' % (lno, row), file=sys.stderr)
+		continue
 	csent = row[2].replace('-','')	
 	csent = csent.replace('[','').replace(']','')
 	print('%s %s\tcro\t%s\n\t   \t%s\n\teng\t%s' % (row[0], row[1], csent, row[2], row[3]))
 	print()
+	lno+=1
